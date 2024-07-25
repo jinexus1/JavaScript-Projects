a6484687862bffacc84b40ea676fcb7b5403749f
jinexus1 <ap2929470@gmail.com>
2024-07-25 15:40:36 +0530

    01_Weather_App

--- 01_WeatherApp/index.js
+++ 01_WeatherApp/index.js
@@ -4,26 +4,33 @@ const apiUrl =
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
-async function checkWeather(city){
-  const response=await fetch(apiUrl + city + `&appid=${apiKey}`)
-  var data=await response.json();
-  console.log(data);
-  document.querySelector(".city").innerHTML=data.name
-  document.querySelector(".temp").innerHTML =Math.round(data.main.temp) + "°C";
-  document.querySelector(".humidity").innerHTML = data.main.humidity;
-  document.querySelector(".wind").innerHTML = data.wind.speed+"Km/h";
-  //text updated
-  //now lets update the image
-  const weatherIcons = {
-    Clouds: "./images/clouds.png",
-    Clear: "./images/clear.png",
-    Rain: "./images/rain.png",
-    Drizzle: "./images/drizzle.png",
-    Mist: "./images/mist.png",
-  };
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  } else {
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+    //text updated
+    //now lets update the image
+    const weatherIcons = {
+      Clouds: "./images/clouds.png",
+      Clear: "./images/clear.png",
+      Rain: "./images/rain.png",
+      Drizzle: "./images/drizzle.png",
+      Mist: "./images/mist.png",
+    };
 
-  weatherIcon.src = weatherIcons[data.weather[0].main];
+    weatherIcon.src = weatherIcons[data.weather[0].main];
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display="none";
+  }
 }
-searchButton.addEventListener("click",()=>{
-  checkWeather(searchBox.value)
-})
+searchButton.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
